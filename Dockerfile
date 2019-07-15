6206bf5ee7189e97e703c0501e03629ff3c17a80
Nicolas Beguier <nicolas.beguier@schibsted.com>
2019-07-15 16:12:02 +0200

    gunicorn and remove duplicate in configuration, fix and health endpoint

--- Dockerfile
+++ Dockerfile
@@ -7,21 +7,13 @@ RUN apk add --update \
     py3-pip \
     gcc \
     musl-dev \
-    openssl \
   && rm -rf /var/cache/apk/*
 
 COPY . .
+COPY settings.conf.example settings.conf
+
 RUN pip3 install -U pip
 RUN pip3 install -r requirements.txt
 
-# Gen certs
-RUN mkdir -p /etc/ssl/certs; \
-    mkdir -p /etc/ssl/private; \
-    openssl genrsa -out /etc/ssl/private/ssl-cert-snakeoil.key 2048; \
-    cp /etc/ssl/private/ssl-cert-snakeoil.key /etc/ssl/private/ssl-cert-snakeoil.key.orig; \
-    openssl rsa -in /etc/ssl/private/ssl-cert-snakeoil.key.orig -out /etc/ssl/private/ssl-cert-snakeoil.key; \
-    openssl req -new -key /etc/ssl/private/ssl-cert-snakeoil.key -out /etc/ssl/certs/cert.csr -subj "/C=GB/ST=GB/L=London/O=DockerTest/OU=DockerTest/CN=DockerTest"; \
-    openssl x509 -req -days 3650 -in /etc/ssl/certs/cert.csr -signkey /etc/ssl/private/ssl-cert-snakeoil.key -out /etc/ssl/certs/ssl-cert-snakeoil.pem
-
-EXPOSE 8443
-CMD ["/usr/bin/python3", "main.py"]
+EXPOSE 8000
+CMD ["/usr/bin/gunicorn", "--workers=2", "--bind=0.0.0.0:8000", "--name=csp-endpoint", "main:APP"]
--- main.py
+++ main.py
@@ -2,58 +2,39 @@
 """ CSP Report """
 
 # Standard library imports
-from argparse import ArgumentParser
 from urllib.parse import urlparse
 import datetime
 import html
-import os
 import json
-from ssl import PROTOCOL_TLSv1_2, SSLContext
+import logging
+import os
 
 # Third party library imports
-from configparser import ConfigParser, NoOptionError, NoSectionError
+from configparser import ConfigParser, NoOptionError
 from flask import Flask, jsonify, abort, make_response, request
 from pymongo import MongoClient
 
 # Debug
 # from pdb import set_trace as st
 
+VERSION = "%(prog)s 1.2.0"
 APP = Flask(__name__)
-VERSION = "%(prog)s 1.1.0"
+REPORT_API_PATH = "/"
+if "REPORT_API_PATH" in os.environ:
+    REPORT_API_PATH = os.environ["REPORT_API_PATH"]
 
 def read_conf(conf_path):
     """
     Read CASSH configuration file and return metadata.
     """
+
+    if not os.path.isfile(conf_path):
+        LOG.error("Can't read configuration file... ({})".format(conf_path))
+        exit(1)
+
     config = ConfigParser()
     config.read(conf_path)
     options = dict()
-    options["main"] = dict()
-
-    try:
-        options["main"]["port"] = int(config.get("main", "port"))
-        options["main"]["host"] = config.get("main", "host")
-        options["main"]["debug"] = config.get("main", "debug") == "True"
-    except (NoOptionError, ValueError) as error_msg:
-        print("Can\"t read configuration file...")
-        print(error_msg)
-        exit(1)
-
-    options["https"] = dict()
-    if not config.has_option("https", "enable"):
-        options["https"]["enable"] = False
-    else:
-        options["https"]["enable"] = config.get("https", "enable") == "True"
-    if config.has_option("https", "pubkey"):
-        options["https"]["pubkey"] = config.get("https", "pubkey")
-        if not os.path.isfile(options["https"]["pubkey"]):
-            print("{} is not a file".format(options["https"]["pubkey"]))
-            exit(1)
-    if config.has_option("https", "privkey"):
-        options["https"]["privkey"] = config.get("https", "privkey")
-        if not os.path.isfile(options["https"]["privkey"]):
-            print("{} is not a file".format(options["https"]["privkey"]))
-            exit(1)
 
     options["mongodb"] = dict()
     if not config.has_option("mongodb", "enable"):
@@ -71,8 +52,7 @@ def read_conf(conf_path):
             options["mongodb"]["pass"] = None
         options["mongodb"]["database"] = config.get("mongodb", "database")
     except (NoOptionError, ValueError) as error_msg:
-        print("Can\"t read configuration file...")
-        print(error_msg)
+        LOG.error("Can't read configuration file... ({})".format(error_msg))
         exit(1)
 
     return options
@@ -98,21 +78,21 @@ def error_405(error):
     }), 405)
 
 
-@APP.route("/", methods=["POST"])
+@APP.route(REPORT_API_PATH, methods=["POST"])
 def csp_receiver():
     ## https://junxiandoc.readthedocs.io/en/latest/docs/flask/flask_request_response.html
     if request.content_type != "application/csp-report":
         abort(400)
 
     csp_report = json.loads(request.data.decode("UTF-8"))["csp-report"]
-    print(datetime.datetime.now(), request.remote_addr, request.content_type, csp_report)
+    LOG.warning("{} - - [{}] Content-Type:'{}', Report:'{}'".format(request.remote_addr, datetime.datetime.now(), request.content_type, csp_report))
 
     blocked_uri = html.escape(csp_report["blocked-uri"], quote=True).split(" ", 1)[0]
     document_uri = html.escape(csp_report["document-uri"], quote=True).split(" ", 1)[0]
     violated_directive = html.escape(csp_report["violated-directive"], quote=True).split(" ", 1)[0]
     #disposition = html.escape(csp_report["disposition"], quote=True)
 
-    if blocked_uri == "about":
+    if blocked_uri == "about" or document_uri == "about":
         return make_response("", 204)
 
     elif not blocked_uri:
@@ -121,6 +101,7 @@ def csp_receiver():
 
         elif violated_directive == "style-src":
             blocked_uri = "inline"
+
     if OPTIONS["mongodb"]["enable"]:
         domain = urlparse(document_uri).hostname
         collection = DB[domain]
@@ -137,32 +118,16 @@ def csp_receiver():
 
     return make_response("", 204)
 
+@APP.route("/health")
+def health():
+    result = {"name": "csp-report", "version": VERSION.split(" ")[1]}
+    return make_response(json.dumps(result), 200)
+
+LOG = logging.getLogger("werkzeug")
+OPTIONS = read_conf("settings.conf")
+MONGO_CONNECTION_STRING = "mongodb://{}:{}".format(OPTIONS["mongodb"]["host"], OPTIONS["mongodb"]["port"])
+CLIENT = MongoClient(MONGO_CONNECTION_STRING, username=OPTIONS["mongodb"]["user"], password=OPTIONS["mongodb"]["pass"])
+DB = CLIENT[OPTIONS["mongodb"]["database"]]
 
 if __name__ == "__main__":
-
-    PARSER = ArgumentParser()
-    PARSER.add_argument("--version", action="version", version=VERSION)
-    PARSER.add_argument("-c", "--conf", action="store",\
-        help="Configuration file.")
-
-    ARGS = PARSER.parse_args()
-
-    if ARGS.conf is None:
-        print("You have to specify a configuration file")
-        PARSER.print_help()
-        exit(1)
-
-    OPTIONS = read_conf(ARGS.conf)
-
-    if OPTIONS["mongodb"]["enable"]:
-        MONGO_CONNECTION_STRING = "mongodb://{}:{}".format(OPTIONS["mongodb"]["host"], OPTIONS["mongodb"]["port"])
-        CLIENT = MongoClient(MONGO_CONNECTION_STRING, username=OPTIONS["mongodb"]["user"], password=OPTIONS["mongodb"]["pass"])
-        DB = CLIENT[OPTIONS["mongodb"]["database"]]
-
-    if OPTIONS["https"]["enable"]:
-        CONTEXT = SSLContext(PROTOCOL_TLSv1_2)
-        CONTEXT.load_cert_chain(OPTIONS["https"]["pubkey"], OPTIONS["https"]["privkey"])
-        APP.run(debug=OPTIONS["main"]["debug"], host=OPTIONS["main"]["host"], port=OPTIONS["main"]["port"], ssl_context=CONTEXT)
-    else:
-        APP.run(debug=OPTIONS["main"]["debug"], host=OPTIONS["main"]["host"], port=OPTIONS["main"]["port"])
-
+    APP.run("0.0.0.0")
