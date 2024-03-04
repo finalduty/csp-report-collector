4497770a609bfbff8421d0023ffac6ae10b9aa64
Andy Dustin <finalduty@users.noreply.github.com>
2024-03-04 01:09:59 +0000

    Added tests, updated bits

--- src/csp_report_collector.py
+++ src/csp_report_collector.py
@@ -9,13 +9,11 @@ from urllib.parse import urlparse
 
 import dotenv
 from flask import Flask, abort, jsonify, make_response, request
-from sqlalchemy.dialects import __all__ as SQLALCHEMY_DIALECTS
 
 __version__ = "0.4.0"
 
 SUPPORTED_DB_ENGINES = [
     "mariadb",
-    "mongodb",
     "mssql",
     "mysql",
     "postgresql",
@@ -38,12 +36,12 @@ def _load_config(config_path: Optional[str] = "settings.conf", env_prefix: str =
     ## Initialise supported variables
     config: dict[str, Optional[str]] = {
         "db_uri": None,
-        "db_engine": None,
+        "db_type": None,
         "db_host": None,
         "db_port": None,
         "db_username": None,
         "db_password": None,
-        "db_path": None,
+        "db_name": None,
     }
 
     ## Load config from file
@@ -64,9 +62,18 @@ def _load_config(config_path: Optional[str] = "settings.conf", env_prefix: str =
         if envvar in os.environ:
             config.update({key: os.environ[envvar]})
 
-    ## If a DB URI is provided, extract and store it's engine as well
+    ## If a DB URI is provided, extract it's components and store them
     if config["db_uri"]:
-        config["db_engine"] = config["db_uri"].split("://")[0]
+        from sqlalchemy import make_url
+
+        db_uri = make_url(config["db_uri"])
+
+        config["db_type"] = db_uri.drivername if db_uri.drivername else config["db_type"]
+        config["db_host"] = db_uri.host if db_uri.drivername else config["db_host"]
+        config["db_port"] = str(db_uri.port) if db_uri.port else config["db_port"]
+        config["db_username"] = db_uri.username if db_uri.username else config["db_username"]
+        config["db_password"] = db_uri.password if db_uri.password else config["db_password"]
+        config["db_name"] = db_uri.database if db_uri.database else config["db_name"]
 
     return config
 
@@ -78,59 +85,39 @@ app.config.update(_load_config())
 ## Set up DB connections
 db = None
 
-if app.config["db_engine"]:
-    if app.config["db_engine"] == "mongodb":
-        from pymongo import MongoClient
+if app.config["db_type"]:
+    from flask_sqlalchemy import SQLAlchemy
+    from sqlalchemy import URL
+    from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-        mongo_client = MongoClient(
-            host=app.config["db_uri"] if app.config["db_uri"] is not None else app.config["db_host"],
-            port=int(app.config["db_port"]),
-            username=app.config["db_username"],
-            password=app.config["db_password"],
-        )
+    class BaseModel(DeclarativeBase):
+        __abstract__ = True  # So SQLAlchemy doesn't create this as a table
+        pass
 
-        db = mongo_client[app.config["db_name"]]
+    class ReportsModel(BaseModel):
+        __tablename__ = "reports"
 
-    elif app.config["db_engine"] in SQLALCHEMY_DIALECTS:
-        from flask_sqlalchemy import SQLAlchemy
-        from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+        id: Mapped[int] = mapped_column(primary_key=True)
+        domain: Mapped[str] = mapped_column(nullable=False, index=True)
+        document_uri: Mapped[str] = mapped_column(nullable=False, index=True)
+        blocked_uri: Mapped[str] = mapped_column(nullable=False, index=True)
+        violated_directive: Mapped[str] = mapped_column(nullable=False, index=True)
+        reported_at: Mapped[datetime] = mapped_column(nullable=False)
 
-        class Base(DeclarativeBase):
-            __abstract__ = True  # So SQLAlchemy doesn't create this as a table
-            pass
+    app.config["SQLALCHEMY_DATABASE_URI"] = URL.create(
+        drivername=app.config["db_type"],
+        username=app.config["db_username"],
+        password=app.config["db_password"],
+        host=app.config["db_host"],
+        port=app.config["db_port"],
+        database=app.config["db_name"],
+    )
 
-        class Reports(Base):
-            __tablename__ = "reports"
+    db = SQLAlchemy(model_class=BaseModel)
+    db.init_app(app)
 
-            id: Mapped[int] = mapped_column(primary_key=True)
-            domain: Mapped[str] = mapped_column(nullable=False, index=True)
-            document_uri: Mapped[str] = mapped_column(nullable=False, index=True)
-            blocked_uri: Mapped[str] = mapped_column(nullable=False, index=True)
-            violated_directive: Mapped[str] = mapped_column(nullable=False, index=True)
-            reported_at: Mapped[datetime] = mapped_column(nullable=False)
-
-        if app.config["db_uri"]:
-            app.config["SQLALCHEMY_DATABASE_URI"] = app.config["db_uri"]
-        else:
-            from sqlalchemy import URL
-
-            app.config["SQLALCHEMY_DATABASE_URI"] = URL.create(
-                drivername=app.config["db_engine"],
-                username=app.config["db_username"],
-                password=app.config["db_password"],
-                host=app.config["db_host"],
-                port=app.config["db_port"],
-                database=app.config["db_name"],
-            )
-
-        db = SQLAlchemy(model_class=Base)
-        db.init_app(app)
-
-        with app.app_context():
-            db.create_all()
-
-    else:
-        raise ValueError(f"Unsupported database engine '{app.config['db_engine']}'. Please choose one of the supported engines: {', '.join(SUPPORTED_DB_ENGINES)}")
+    with app.app_context():
+        db.create_all()
 
 app.config["db"] = db
 
@@ -156,19 +143,24 @@ def error_405(error):
 def csp_receiver():
     ## https://junxiandoc.readthedocs.io/en/latest/docs/flask/flask_request_response.html
     if request.content_type != "application/csp-report":
-        abort(400)
+        abort(400, f"Invalid content type. Expected 'application/csp-report', got '{request.content_type}'.")
 
     csp_report = json.loads(request.data.decode("UTF-8"))["csp-report"]
     log.info(f"{datetime.now()} {request.remote_addr} {request.content_type} {csp_report}")
 
-    blocked_uri = html.escape(csp_report["blocked-uri"], quote=True).split(" ", 1)[0]
+    try:
+        blocked_uri = html.escape(csp_report["blocked-uri"], quote=True).split(" ", 1)[0]
+    except AttributeError:
+        blocked_uri = None
+
     document_uri = html.escape(csp_report["document-uri"], quote=True).split(" ", 1)[0]
     violated_directive = html.escape(csp_report["violated-directive"], quote=True).split(" ", 1)[0]
 
     domain = urlparse(document_uri).hostname
 
+    ## Short-ciruit reports for 'about' pages, i.e. about:config, etc.
     if blocked_uri == "about" or document_uri == "about":
-        return make_response("", 204)
+        return make_response(jsonify({}), 204)
 
     elif not blocked_uri:
         if violated_directive == "script-src":
@@ -177,35 +169,28 @@ def csp_receiver():
         elif violated_directive == "style-src":
             blocked_uri = "inline"
 
-    _write_to_sql(
-        reported_at=datetime.utcnow(),
-        domain=domain,
-        document_uri=document_uri,
-        blocked_uri=blocked_uri,
-        violated_directive=violated_directive,
-    )
+    if app.config["db"]:
+        _write_to_database(
+            db=app.config["db"],
+            reported_at=datetime.utcnow(),
+            domain=domain,
+            document_uri=document_uri,
+            blocked_uri=str(blocked_uri),
+            violated_directive=violated_directive,
+        )
 
-    return make_response("", 204)
+    return make_response(jsonify({}), 204)
 
 
-def _write_to_storage(
+def _write_to_database(
+    db: SQLAlchemy,
     domain: str,
-    document_uri: str,
     blocked_uri: str,
-    violated_directive: str,
-    reported_at: datetime,
-) -> None:
-    pass
-
-
-def _write_to_sql(
-    domain: str,
     document_uri: str,
-    blocked_uri: str,
-    violated_directive: str,
     reported_at: datetime,
+    violated_directive: str,
 ) -> None:
-    report = Reports(
+    report = ReportsModel(
         domain=domain,
         document_uri=document_uri,
         blocked_uri=blocked_uri,
@@ -217,11 +202,10 @@ def _write_to_sql(
     db.session.commit()
 
 
-@app.route("/health")
-def health():
-    result = {"name": "csp-report", "version": __version__}
-    return make_response(json.dumps(result), 200)
+@app.route("/status")
+def status():
+    return make_response("ok", 200)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: nocover
     app.run(host="127.0.0.1")
