7c14d099297595dfed8c54b0773a458a4a40c65f
zeroschism <zeroschism@users.noreply.github.com>
2025-03-24 16:44:55 +0000

    tests: refactor tests to reflect new design

--- tests/test_csp_reports.py
+++ tests/test_csp_reports.py
@@ -1,82 +1,8 @@
 import csp_reports
 import csp_datamodel
-import json
 import pytest
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-@pytest.fixture
-def empty():
-    return ""
-
-@pytest.fixture
-def none():
-    return None
-
-@pytest.fixture(scope="module")
-def legacy_report_type():
-    return "csp-report"
-
-@pytest.fixture(scope="module")
-def report_type():
-    return "csp-violation"
-
-@pytest.fixture(scope="module")
-def user_agent():
-    return "Mozilla/5.0 - pytest"
-
-@pytest.fixture(scope="module")
-def document_url():
-    return "https://pytest.test-domain.com/test"
-
-@pytest.fixture(scope="module")
-def blocked_url():
-    return "https://www.evil.com/payload/evil.js"
-
-@pytest.fixture(scope="module")
-def effective_directive():
-    return "inline-src"
-
-@pytest.fixture(scope="module")
-def original_policy():
-    return "default-src 'self';"
-
-@pytest.fixture(scope="module")
-def referrer():
-    return "https://www.duckduckgo.com"
-
-@pytest.fixture(scope="module")
-def source_file():
-    return "https://my-domain.com/about"
-
-@pytest.fixture(scope="module")
-def disposition():
-    return "enforce"
-
-@pytest.fixture(scope="module")
-def column_number():
-    return "39"
-
-@pytest.fixture(scope="module")
-def line_number():
-    return "17"
-
-@pytest.fixture(scope="module")
-def sample():
-    return "<javascript>alert(1);</javascript>"
-
-@pytest.fixture(scope="module")
-def status_code():
-    return "200"
-
-@pytest.fixture(scope="function")
-def get_db():
-    engine = create_engine("sqlite:///:memory:")
-    csp_datamodel.ReportsModel.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine,expire_on_commit=False)
-    with Session() as db:
-        yield db
+from conftest import FixedData
 
 def generate_report_to(*args) -> dict:
     report_to_keys = [
@@ -155,12 +81,38 @@ def generate_report_uri(*args) -> dict:
     return report_uri
 
 @pytest.fixture(scope="function")
-def report_to(report_type,user_agent, document_url, blocked_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code):
-    return generate_report_to(report_type,user_agent, document_url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code)
+def report_to():
+    return generate_report_to(
+        FixedData.report_type("preferred"),
+        FixedData.user_agent(),
+        FixedData.document_url(),
+        FixedData.blocked_url(),
+        FixedData.document_url(), 
+        FixedData.effective_directive(),
+        FixedData.original_policy(),
+        FixedData.referrer(),
+        FixedData.disposition(),
+        FixedData.source_file(),
+        FixedData.column_number(),
+        FixedData.line_number(),
+        FixedData.code_sample(),
+        FixedData.status_code()
+    )
 
 @pytest.fixture(scope="function")
-def report_uri(legacy_report_type,blocked_url, document_url, disposition, effective_directive, original_policy, referrer, sample, status_code):
-    return generate_report_uri(legacy_report_type,blocked_url, document_url, disposition, effective_directive, effective_directive, original_policy, referrer, sample, status_code)
+def report_uri():
+    return generate_report_uri(
+        FixedData.report_type("legacy"),
+        FixedData.blocked_url(),
+        FixedData.document_url(),
+        FixedData.disposition(),
+        FixedData.effective_directive(),
+        FixedData.effective_directive(),
+        FixedData.original_policy(),
+        FixedData.referrer(),
+        FixedData.code_sample(),
+        FixedData.status_code()
+    )
 
 def test_report_factory_reportto(report_to):
     print(f"report to: {report_to}")
@@ -196,13 +148,12 @@ def test_write_database(report_to,get_db):
         "status_code"
     ],
     [
-        pytest.param(none, blocked_url, document_url, disposition, effective_directive, effective_directive, original_policy, referrer, sample, status_code, id="missing-type"),
-        pytest.param(legacy_report_type, none, document_url, disposition, effective_directive, effective_directive, original_policy, referrer, sample, status_code, id="missing-blocked-url"),
-        pytest.param(legacy_report_type, blocked_url, none, disposition, effective_directive, effective_directive, original_policy, referrer, sample, status_code, id="missing-document-url"),
-        pytest.param(legacy_report_type, blocked_url, document_url, none, effective_directive, effective_directive, original_policy, referrer, sample, status_code, id="missing-disposition"),
-        pytest.param(legacy_report_type, blocked_url, document_url, disposition, none, none, original_policy, referrer, sample, status_code, id="missing-effective-directive"),
-        pytest.param(legacy_report_type, blocked_url, document_url, disposition, effective_directive, effective_directive, none, referrer, sample, status_code, id="missing-policy"),
-        pytest.param(legacy_report_type, blocked_url, document_url, disposition, effective_directive, effective_directive, original_policy, referrer, sample, none, id="missing-code"),
+        pytest.param(FixedData.none(), FixedData.blocked_url(), FixedData.document_url(), FixedData.disposition(), FixedData.effective_directive(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code(), id="missing-type"),
+        pytest.param(FixedData.report_type("legacy"), FixedData.blocked_url(), FixedData.none(), FixedData.disposition(), FixedData.effective_directive(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code(), id="missing-document-url"),
+        pytest.param(FixedData.report_type("legacy"), FixedData.blocked_url(), FixedData.document_url(), FixedData.none(), FixedData.effective_directive(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code(), id="missing-disposition"),
+        pytest.param(FixedData.report_type("legacy"), FixedData.blocked_url(), FixedData.document_url(), FixedData.disposition(), FixedData.none(), FixedData.none(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code(), id="missing-effective-directive"),
+        pytest.param(FixedData.report_type("legacy"), FixedData.blocked_url(), FixedData.document_url(), FixedData.disposition(), FixedData.effective_directive(), FixedData.effective_directive(), FixedData.none(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code(), id="missing-policy"),
+        pytest.param(FixedData.report_type("legacy"), FixedData.blocked_url(), FixedData.document_url(), FixedData.disposition(), FixedData.effective_directive(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.none(), id="missing-code"),
     ]
 )
 def test_validate_required_elements_legacy(legacy_report_type, blocked_url, document_url, disposition, effective_directive, violated_directive, original_policy, referrer, script, status_code):
@@ -223,29 +174,66 @@ def test_validate_required_elements_legacy(legacy_report_type, blocked_url, docu
         "source_file",
         "column_number",
         "line_number",
+        "script",
+        "status_code"
+    ],
+    [
+        pytest.param(FixedData.none(), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.document_url(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code(), id="missing-type"),
+        pytest.param(FixedData.report_type("preferred"), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.none(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code(), id="missing-document-url"),
+        pytest.param(FixedData.report_type("preferred"), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.document_url(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.none(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code(), id="missing-disposition"),
+        pytest.param(FixedData.report_type("preferred"), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.document_url(), FixedData.none(), FixedData.original_policy(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code(), id="missing-effective-directive"),
+        pytest.param(FixedData.report_type("preferred"), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.document_url(), FixedData.effective_directive(), FixedData.none(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code(), id="missing-policy"),
+        pytest.param(FixedData.report_type("preferred"), FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.document_url(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.none(), id="missing-code"),
+    ]
+)
+def test_validate_required_elements(report_type, user_agent, url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, script, status_code):
+    with pytest.raises(csp_reports.RequiredElementMissingError):
+        report = csp_reports.get_report(generate_report_to(report_type, user_agent, url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, script, status_code))
+
+@pytest.mark.parametrize(
+    [
+        "document_url",
+        "blocked_url",
+        "disposition",
+        "effective_directive",
+        "original_policy",
+        "referrer",
         "sample",
         "status_code"
     ],
     [
-        pytest.param(none, user_agent, document_url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code, id="missing-type"),
-        pytest.param(report_type, user_agent, document_url, none, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code, id="missing-blocked-url"),
-        pytest.param(report_type, user_agent, document_url, blocked_url, none, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code, id="missing-document-url"),
-        pytest.param(report_type, user_agent, document_url, blocked_url, document_url, effective_directive, original_policy, referrer, none, source_file, column_number, line_number, sample, status_code, id="missing-disposition"),
-        pytest.param(report_type, user_agent, document_url, blocked_url, document_url, none, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code, id="missing-effective-directive"),
-        pytest.param(report_type, user_agent, document_url, blocked_url, document_url, effective_directive, none, referrer, disposition, source_file, column_number, line_number, sample, status_code, id="missing-policy"),
-        pytest.param(report_type, user_agent, document_url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, none, id="missing-code"),
+        (FixedData.document_url(), FixedData.blocked_url(), FixedData.disposition(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.code_sample(), FixedData.status_code())
     ]
 )
-def test_validate_required_elements(report_type, user_agent, url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code):
-    with pytest.raises(csp_reports.RequiredElementMissingError):
-        report = csp_reports.get_report(generate_report_to(report_type, user_agent, url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code))
-
-def test_invalid_report_type_legacy():
+def test_invalid_report_type_legacy(document_url, blocked_url, disposition, effective_directive, original_policy, referrer, sample, status_code):
     with pytest.raises(csp_reports.InvalidReportType):
         invalid_report_type = "invalid-type"
         report = csp_reports.get_report(generate_report_uri(invalid_report_type,blocked_url, document_url, disposition, effective_directive, effective_directive, original_policy, referrer, sample, status_code))
 
-def test_invalid_report_type():
+@pytest.mark.parametrize(
+        [
+            "user_agent",
+            "document_url",
+            "blocked_url",
+            "effective_directive",
+            "original_policy",
+            "referrer",
+            "disposition",
+            "source_file",
+            "column_number",
+            "line_number",
+            "sample",
+            "status_code"
+        ],
+        [
+            (FixedData.user_agent(), FixedData.document_url(), FixedData.blocked_url(), FixedData.effective_directive(), FixedData.original_policy(), FixedData.referrer(), FixedData.disposition(), FixedData.source_file(), FixedData.column_number(), FixedData.line_number(), FixedData.code_sample(), FixedData.status_code())
+        ]
+)
+def test_invalid_report_type(user_agent, document_url, blocked_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code):
     with pytest.raises(csp_reports.InvalidReportType):
         invalid_report_type = "invalid-type"
         report = csp_reports.get_report(generate_report_to(invalid_report_type, user_agent, document_url, blocked_url, document_url, effective_directive, original_policy, referrer, disposition, source_file, column_number, line_number, sample, status_code))
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-vvv", "--no-cov"])
